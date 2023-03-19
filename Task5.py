b9f10553b07cb5c50ceebebc9009bb12eef7a011
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-03-19 20:06:17 +0300

    190323_20-06

--- Task5.py
+++ Task5.py
@@ -38,7 +38,7 @@ def create_db(conn):
         cur.execute("""
         CREATE TABLE IF NOT EXISTS phone(
             phone_id SERIAL PRIMARY KEY,
-            phone INTEGER UNIQUE
+            phone VARCHAR(20) UNIQUE
         );
         """)
    
@@ -63,25 +63,78 @@ def create_db(conn):
 
 
 
-def add_client(conn, first_name, last_name, email, phones=None):
-    print(first_name, last_name, email, phones)
+def add_customer(conn, first_name, last_name, email, phones=None):
+    with conn.cursor() as cur:
+        cur.execute("""
+        INSERT INTO name(name) VALUES(%s) ON CONFLICT DO NOTHING;
+        """,(first_name,))
+        cur.execute("""
+        INSERT INTO surname(surname) VALUES(%s) ON CONFLICT DO NOTHING;
+        """,(last_name,))
+        cur.execute("""
+        INSERT INTO email(email) VALUES(%s) ON CONFLICT DO NOTHING;
+        """,(email,))
+        conn.commit()
+
+        if phones == 0:
+            cur.execute("""
+            INSERT INTO phone(phone) VALUES(%s) ON CONFLICT DO NOTHING;
+            """,(0,))
+        else:
+            for i in phones:
+                cur.execute("""
+                INSERT INTO phone(phone) VALUES(%s) ON CONFLICT DO NOTHING;
+                """,(i,))
+        conn.commit()
+
+        cur.execute(""" 
+        SELECT name_id FROM name WHERE name=%s;
+        """,(first_name,))
+        name_id = cur.fetchone()[0]
+
+        cur.execute(""" 
+        SELECT surname_id FROM surname WHERE surname=%s;
+        """,(last_name,))
+        surname_id = cur.fetchone()[0]
+
+        cur.execute(""" 
+        SELECT email_id FROM email WHERE email=%s;
+        """,(email,))
+        email_id = cur.fetchone()[0]
+        
+
+        cur.execute("""
+        INSERT INTO customer(customer_name, customer_surname, customer_email) VALUES(%s,%s,%s);
+        """,(name_id,surname_id, email_id))
+
+        conn.commit()
+        
+
+        
+
+        
+        
+        
+        #INSERT INTO phone_customer(customer_id) VALUES(SELECT customer_id FROM customer JOIN email ON customer(customer_email) = email(email_id) WHERE email=email);
+        #INSERT INTO phone_customer(phone_id)
+        
 
 def add_phone(conn, client_id, phone):
     pass
 
-def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+def change_customer(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
     pass
 
 def delete_phone(conn, client_id, phone):
     pass
 
-def delete_client(conn, client_id):
+def delete_customer(conn, client_id):
     pass
 
-def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
+def find_customer(conn, first_name=None, last_name=None, email=None, phone=None):
     pass
 
-def get_client_name():
+def get_customer_name():
     while True:
         name = input('Введите имя клиента (поле не может быть пустым):').lstrip().rstrip()
         if name:
@@ -90,7 +143,7 @@ def get_client_name():
             continue
     return name
 
-def get_client_surname():
+def get_customer_surname():
     while True:
         surname = input('Введите фамилию клиента (поле не может быть пустым):').lstrip().rstrip()
         if surname:
@@ -99,7 +152,7 @@ def get_client_surname():
             continue
     return surname
 
-def get_client_email():
+def get_customer_email():
     while True:
         email = input('Введите email клиента (поле не может быть пустым):').lstrip().rstrip()
         if email and '@' in email:
@@ -108,9 +161,9 @@ def get_client_email():
             continue
     return email
 
-def get_client_phones():
+def get_customer_phones():
     number_of_phones = int(input('Введите кол-во телефонных номеров клиента:'))
-    client_phones = ()
+    client_phones = []
     if number_of_phones == 0:
         return 0
     else:
@@ -129,7 +182,8 @@ def get_client_phones():
 with psycopg2.connect(database="task5", user="postgres", password="Maxim0055!!!") as conn:
     remove_db(conn)
     create_db(conn)
-    add_client(conn, get_client_name(), get_client_surname(), get_client_email(), get_client_phones())
+    for i in range(3):
+        add_customer(conn, get_customer_name(), get_customer_surname(), get_customer_email(), get_customer_phones())
     
 conn.close()
 
